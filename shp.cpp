bb22b407d7c5e4829bec9062e749ba86386c98e0
jonapoul <jonathanpoulton1@gmail.com>
2017-01-20 13:20:39 +0000

    Removed simulation files

--- shp.cpp
+++ shp.cpp
@@ -1,7 +1,8 @@
 #include "headers/Plate.h"
-#include "headers/Particle.h"
 #include <fstream>
 #include <vector>
+#include <cctype>
+#include <stdio.h>
 using std::cout;
 
 int main(int argc, char* argv[]) {
@@ -21,8 +22,25 @@ int main(int argc, char* argv[]) {
 		file.close();
 	}
 
-	Particle p{"Earth", {}, {}, 1.f, 6371e3};
-	cout << p.toString() << '\n';
+	std::ifstream ephemerisFile("mars_ephemeris.txt");
+	if (ephemerisFile.is_open()) {
+		while (!ephemerisFile.eof()) {
+			std::string buffer;
+			getline(ephemerisFile, buffer);
+			if (buffer.length() > 5) {
+				std::string dateStr = buffer.substr(1,4);
+				auto isAllDigits = [](std::string s) {
+					for (auto c : s)
+						if (!isdigit(c))	return false;
+					return true;
+				};
+				if ( isAllDigits(dateStr) ) {
+					//cout << dateStr << '\n';
+				}
+			}
+		}
+		ephemerisFile.close();
+	}
 
 	// for (auto p : plates) {
 	// 	///////////////////////////////////////////////////////////////
@@ -36,6 +54,6 @@ int main(int argc, char* argv[]) {
 
 // carry on with reading an ephemeris line from the mars file
 // also find a good timespace between records, is 4h too much? Jumps by ~2arcmin per record for DEC 
-// carry on with Vector and Particle
+// carry on with Particle simulation functions
 // check for the word "TEST", so the plate can be discarded. It comes up a lot in the last catalog file
 // test gnomonic projection, how fast/efficient is it?
